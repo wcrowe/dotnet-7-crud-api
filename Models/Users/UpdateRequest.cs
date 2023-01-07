b24094e975e38e3364dd0e3df1326f8450b19ae9
wcrowe <crowe.will@gmail.com>
2023-01-07 13:06:00 -0500

    cleanup

--- Models/Users/UpdateRequest.cs
+++ Models/Users/UpdateRequest.cs
@@ -32,10 +32,8 @@ public class UpdateRequest
 
     // helpers
 
-    // ReSharper disable once MemberCanBeMadeStatic.Local
-    private string ReplaceEmptyWithNull(string? value)
+    private static string ReplaceEmptyWithNull(string? value)
     {
-        // replace empty string with null to make field optional
         return (string.IsNullOrEmpty(value) ? null : value)!;
     }
 }
